cc6a82cbc2923dc95768ee554a1d257f1941e9eb
LuvThakur <luvjadaun786@gmail.com>
2024-01-04 23:55:18 +0530

    remove an Api Key

--- src/components/api/Api.js
+++ src/components/api/Api.js
@@ -2,7 +2,7 @@ export const Geo_Api_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApioptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '51d8bd5b8amsh1c237f7d6bb21cap10c0a4jsn4c03c6eecf0e',
+        'X-RapidAPI-Key': 'Entered Your Api key',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
@@ -11,4 +11,4 @@ export const geoApioptions = {
 export const currweatherURL = "https://api.openweathermap.org/data/2.5";
 export const forcastURL="https://api.openweathermap.org/data/2.5";
 
-export const C_W_API_key = "c09a8d6baf32e8e86a035442a3b13609";
+export const C_W_API_key = "Enter Your Api Key";
